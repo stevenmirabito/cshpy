3a6b4990abe353b8fa111cef948b3b09a5f2e514
Steven Mirabito <steven@stevenmirabito.com>
2016-01-10 10:38:57 -0500

    PEP8, syntax/library updates

--- csh/utils.py
+++ csh/utils.py
@@ -1,13 +1,16 @@
 from datetime import datetime, date
 
+
 def date_from_ldap_timestamp(timestamp):
-    """ Takes an LDAP date (In the form YYYYmmdd
-        with whatever is after that) and returns a
-        datetime.date object.
     """
-    # only check the first 8 characters: YYYYmmdd
-    numberOfCharacters = len("YYYYmmdd")
-    timestamp = timestamp[:numberOfCharacters]
+    Takes an LDAP date (In the form YYYYmmdd with whatever is after that) and returns a datetime.date object.
+    :param timestamp: LDAP date to convert
+    :return: datetime.date object
+    """
+    # Only check the first 8 characters: YYYYmmdd
+    number_of_characters = len("YYYYmmdd")
+    timestamp = timestamp[:number_of_characters]
+
     try:
         day = datetime.strptime(timestamp, '%Y%m%d')
         return date(year=day.year, month=day.month, day=day.day)
